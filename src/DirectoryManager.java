7b1d6011bf763808d1da7f713ebf1c789cd0caf5
neesetifa <36785948+neesetifa@users.noreply.github.com>
2019-01-15 22:20:36 -0800

    Update DirectoryManager.java

--- src/DirectoryManager.java
+++ src/DirectoryManager.java
@@ -1,18 +1,4 @@
 import java.util.*;
-/*
-class DirectoryManager
-{
-	Hashtable<StringBuffer, FileInfo> T = new Hashtable<StringBuffer, FileInfo>();
-	void enter(StringBuffer key, FileInfo file)
-	{
-		T.put(key,file);
-	}
-	FileInfo lookup(StringBuffer key)
-	{
-		return T.get(key);
-	}
-}*/
-
 
 class DirectoryManager
 {
